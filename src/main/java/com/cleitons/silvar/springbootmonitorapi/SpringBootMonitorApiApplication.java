22cbdd5ec95d58560c4055416ebad2fb4adf223e
Cleiton Silva <cleiton_.98@hotmail.com>
2020-08-30 03:36:17 -0300

    Initial commit

---
+++ src/main/java/com/cleitons/silvar/springbootmonitorapi/SpringBootMonitorApiApplication.java
@@ -0,0 +1,13 @@
+package com.cleitons.silvar.springbootmonitorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMonitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMonitorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleitons/silvar/springbootmonitorapi/SpringBootMonitorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleitons.silvar.springbootmonitorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMonitorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
